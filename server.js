f38296eb670e40dee3e0c90f29fb3b1249f8a7c9
mentosu-bule <e1c22065@oit.ac.jp>
2024-12-18 22:23:41 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// 鍵の状態（初期値はnull）
 let currentHolder = null;
 
 app.use(express.json());
 
-// 状態を取得
+// 状態を取得するエンドポイント
 app.get('/key-status', (req, res) => {
     res.json({ currentHolder: currentHolder });
 });
 
-// 状態を更新
+// 状態を更新するエンドポイント
 app.post('/update-key-status', (req, res) => {
-    currentHolder = req.body.currentHolder;
+    currentHolder = req.body.currentHolder; // 新しい借り手の名前を保存
     res.status(200).send("状態が更新されました");
 });
 
